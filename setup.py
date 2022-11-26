6fe2d99b59b4c795f8c13860cb67e0b96ca6476e
ticometiah <119102605+ticometiah@users.noreply.github.com>
2022-11-26 15:28:22 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "harajofil.py",
+    version = "0.1",
+    py_modules = ['harajofil'],
+    url = "https://github.com/harajofil/harajofil.git",
+    author = "harajofil",
+    install_requires = ["pyngrok"]
+)
